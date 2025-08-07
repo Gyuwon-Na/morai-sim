32a4862fe42b360b2a249f46d1aa4f5a485134f5
Gyuwon-Na <uoyuvvou@gmail.com>
2025-08-08 00:28:23 +0900

    장애물 회피

--- src/test/scripts/mission_control.py
+++ src/test/scripts/mission_control.py
@@ -2,21 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+from time import sleep
 
 class MissionControl:
     def __init__(self):
         self.current_mission_idx = 0
         self.current_frame_count = 0
 
-        # straight : 100 per 1 line
         self.mission_list = [
-            {"type": "straight", "duration": 100},
-            {"type": "straight", "duration": 100},
-            {"type": "mission", "duration": 100},
-            {"type": "straight", "duration": 100},
-            {"type": "mission", "duration": 100},
-            {"type": "straight", "duration": 100},
-            {"type": "curve", "duration": 100},
+            {"type": "straight", "duration": 180},
+            {"type": "curve", "duration": 30},
+            {"type": "straight", "duration": 120},
+            {"type": "curve", "duration": 30},
+            {"type": "straight", "duration": 60},
+            {"type": "curve", "duration": 30},
+            {"type": "straight", "duration": 60},
+            {"type": "curve", "duration": 30},
+            {"type": "straight", "duration": 60},
+            {"type": "curve", "duration": 30},
+            {"type": "straight", "duration": 80},
+            {"type": "curve", "duration": 30},
+            {"type": "straight", "duration": 160},
+            {"type": "curve", "duration": 30},
+            {"type": "straight", "duration": 80}
         ]
         self.current_mission_idx = 0
         self.current_frame_count = 0
@@ -25,7 +33,6 @@ class MissionControl:
         if self.mission_list and self.current_mission_idx < len(self.mission_list):
             mission = self.mission_list[self.current_mission_idx]
 
-            # duration만큼 프레임이 지나면 다음 미션으로 이동
             if self.current_frame_count >= mission["duration"]:
                 self.current_frame_count = 0
                 self.current_mission_idx += 1
@@ -37,9 +44,21 @@ class MissionControl:
             else:
                 self.current_frame_count += 1
 
-            # 항상 현재 미션 리턴
             if self.current_mission_idx < len(self.mission_list):
                 return self.mission_list[self.current_mission_idx]
 
         return None
 
+if __name__ == "__main__":
+    rospy.init_node("mission_test_node")
+    mc = MissionControl()
+
+    rate = rospy.Rate(10)  # 10 Hz (0.1초마다 호출)
+    while not rospy.is_shutdown():
+        current_mission = mc.determineMission()
+        if current_mission:
+            rospy.loginfo(f"Current Mission: {current_mission['duration'] - mc.current_frame_count} frames remaining")
+        else:
+            rospy.loginfo("No more missions.")
+            break
+        rate.sleep()
